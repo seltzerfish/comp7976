8e07562331aedf1f4b304b470c041b8ac5f7a1c0
Conner Lane <cal0043@auburn.edu>
2018-09-18 15:28:41 -0500

    add casis scoring

--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
+import os
 from string import printable
 from math import sqrt
 
 def magnitude(vector):
     '''Computes magnitude of vector
-    
+
     Args:
         vector (iterable): an iterable object of numeric contents
-    
+
     Returns:
         float: magnitude
     '''
@@ -16,23 +17,42 @@ def magnitude(vector):
 
 def extract_features_ascii_unigram(input_string):
     '''Extracts feature scores based on single letter occurences of ascii characters.
-    
+
     Args:
         input_string (string): the string to be scored
-    
+
     Returns:
         list: a list of feature scores
     '''
 
+    # ascii_chars = list(printable)[:-5] #TODO: should whitespace and newlines be omitted?
     ascii_chars = list(printable)
     feature_vector = []
     for char in ascii_chars:
         # count occurence of each ascii character
-        feature_vector.append(input_string.count(char)) 
+        feature_vector.append(input_string.count(char))
+
+    return feature_vector
+
+
+def normalize(vector):
+    '''normalizes a vector
     
-    # normalize
-    mag = magnitude(feature_vector)
-    normalized = [x / mag for x in feature_vector]
+    Args:
+        vector (iterable): the vector to be normalized
+    
+    Returns:
+        list: normalized vector
+    '''
 
-    return normalized
 
+    mag = magnitude(vector)
+    return [x / mag for x in vector]
+
+
+def create_folder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from os import listdir
-from helpers import extract_features_ascii_unigram
+from helpers import extract_features_ascii_unigram, create_folder, normalize
 
-files = listdir("data")
+INPUT_FOLDER = "sports_writers"
+OUT_FOLDER = "sports_writers_features"
+
+create_folder(OUT_FOLDER + "-raw")
+create_folder(OUT_FOLDER + "-normalized")
+files = listdir(INPUT_FOLDER)
+
 for file in files:
-    with open("data/" + file, "r") as f:
+    with open(INPUT_FOLDER + "/" + file, "r", encoding='utf-8', errors='ignore') as f:
+        # TODO: should this be converted to all lowercase?
         lines = "\n".join(f.readlines())
         features = extract_features_ascii_unigram(lines)
-        with open("features_unigram/features-" + file, "w") as w:
+        normalized_features = normalize(features)
+        with open(OUT_FOLDER + "-raw/rawcu-" + file, "w") as w:
             w.write(str(features))
+        with open(OUT_FOLDER + "-normalized/ncu-" + file, "w") as w:
+            w.write(str(normalized_features))
+
