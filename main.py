b4f5de9c32f1edb02a9efde1cb2866f0ff07dc6f
Conner Lane <cal0043@auburn.edu>
2018-10-17 19:44:13 -0500

    formatting

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from helpers import *
 from classifiers import k_nearest, k_nearest_weighted, grnn
 
 INPUT_FOLDER = "sports_writers"
+FUNCTIONS = (k_nearest, k_nearest_weighted, grnn)
 
 
 # Our collected sports writers:
-print("\n*** Sports writers ***")
 x_collected, y_collected = load_data_as_x_and_y(INPUT_FOLDER)
-FUNCTIONS = (k_nearest, k_nearest_weighted, grnn)
+print("\nSports writers")
+print("Number of classes: {}".format(len(set(y_collected))))
 for func in FUNCTIONS:
     for k in (1, 3, 5, len(x_collected) - 1):
         print(
-            "{} with k = {}: {:.2f}% accuracy.".format(
+            '\t"{}" with k = {}: {:.2f}% accuracy.'.format(
                 func.__name__,  # function name
                 k,  # used k
                 score_function_accuracy(func, x_collected, y_collected, k) * 100,
@@ -23,12 +24,13 @@ print("\n")
 
 
 # CASIS 25
-print("*** CASIS 25 ***")
 x_given, y_given = load_given_features("hw2_data_ncu.txt")
+print("CASIS 25")
+print("Number of classes: {}".format(len(set(y_given))))
 for func in FUNCTIONS:
     for k in (1, 3, 5, len(x_given) - 1):
         print(
-            "{} with k = {}: {:.2f}% accuracy.".format(
+            '\t"{}" with k = {}: {:.2f}% accuracy.'.format(
                 func.__name__,
                 k,
                 score_function_accuracy(func, x_given, y_given, k) * 100,
